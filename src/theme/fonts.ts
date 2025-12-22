865e2d527590def0175226d4535ceab747485832
Jax Hoang <huynhhq209@gmail.com>
2025-12-22 23:36:38 +0700

    feat: add fonts

---
+++ src/theme/fonts.ts
@@ -0,0 +1,20 @@
+const DEFAULT_FONTS = {
+  SourceSans3Black: 'SourceSans3-Black',
+  SourceSans3BlackItalic: 'SourceSans3-BlackItalic',
+  SourceSans3Bold: 'SourceSans3-Bold',
+  SourceSans3BoldItalic: 'SourceSans3-BoldItalic',
+  SourceSans3ExtraBold: 'SourceSans3-ExtraBold',
+  SourceSans3ExtraBoldItalic: 'SourceSans3-ExtraBoldItalic',
+  SourceSans3ExtraLight: 'SourceSans3-ExtraLight',
+  SourceSans3ExtraLightItalic: 'SourceSans3-ExtraLightItalic',
+  SourceSans3Italic: 'SourceSans3-Italic',
+  SourceSans3Light: 'SourceSans3-Light',
+  SourceSans3LightItalic: 'SourceSans3-LightItalic',
+  SourceSans3Medium: 'SourceSans3-Medium',
+  SourceSans3MediumItalic: 'SourceSans3-MediumItalic',
+  SourceSans3Regular: 'SourceSans3-Regular',
+  SourceSans3SemiBold: 'SourceSans3-SemiBold',
+  SourceSans3SemiBoldItalic: 'SourceSans3-SemiBoldItalic',
+} as const;
+
+export { DEFAULT_FONTS };
