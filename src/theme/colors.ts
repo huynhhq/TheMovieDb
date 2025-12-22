8b1644d027d949f9f6954f45bc080cd1c8fa7599
Jax Hoang <huynhhq209@gmail.com>
2025-12-22 23:42:23 +0700

    feat: add theme

---
+++ src/theme/colors.ts
@@ -0,0 +1,10 @@
+export const colors = {
+  white: '#FFFFFF',
+  black: '#000000',
+  primary_blue: '#00B4E4',
+  border_e3: '#E3E3E3',
+  gray: {
+    100: '#F5F5F5',
+    200: '#F8F8F8',
+  },
+};
