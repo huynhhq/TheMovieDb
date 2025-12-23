e7b49e4f9d2cb0ed44c3d3a1c67081c8310c3b82
Jax Hoang <huynhhq209@gmail.com>
2025-12-24 01:37:44 +0700

    fix: added models

--- src/models/utils.d.ts
+++ src/models/utils.d.ts
@@ -3,3 +3,14 @@ interface ISvgIconProps {
   color?: string;
   strokeWidth?: number;
 }
+
+interface IParams {
+  [key: string]: any;
+}
+
+interface GetItemsResponse<T> {
+  results: T[];
+  page?: number;
+  total_pages: number;
+  total_results: number;
+}
